8e52c010c7a460aabc67da16503df8f226a71a09
Vitalii Lipovetskii <vlipovetskii@users.noreply.github.com>
2024-05-25 19:14:55 +0300

    +hello-world.cbl

---
+++ src/hello-world.cbl
@@ -0,0 +1,8 @@
+		   >>SOURCE FORMAT IS FREE
+IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello-world.
+
+       PROCEDURE DIVISION.
+           DISPLAY "Hello world!".
+           STOP RUN.
+		   
